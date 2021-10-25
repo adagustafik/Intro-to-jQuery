b11c62e9d3bda63c2e4f2785cc1b358c109aec78
adagustafik <ada.gustafik@gmail.com>
2021-10-25 18:03:22 +0200

    Finished exercise 2

---
+++ script.js
@@ -0,0 +1,16 @@
+let color = 'purple';
+let number = 10;
+let word = 'cool';
+
+$('.north').css('background-color', 'purple');
+if (number > 100) {
+        $('.east').text("whoah, that's a big number");
+    } else {
+        $('.east').text("just a regular number, please");
+    };
+
+if (word === 'cool') {
+        $('.south').text("Power of DOM");
+} else {
+        $('.west').text("Power of DOM");
+}
